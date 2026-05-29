2d9b4da49d097168d89dcba69c39e4f8ccc74c62
Mallikharjuna Korrapati <mallik2026rao@gmail.com>
2026-05-29 17:53:35 +0530

    FINAL FIX2

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('✅ Hello from AKS - SUCCESS!');
+    res.end('✅ Hello from AKS - SUCCESS');
 });
 
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
+
+// ✅ THIS LINE FIXES YOUR ISSUE
+setInterval(() => {
+    console.log("App is running...");
+}, 5000);
