26f0aa7dbb4a8c9310110bad927a068e8f94d6e7
Mallikharjuna Korrapati <mallik2026rao@gmail.com>
2026-05-29 17:07:38 +0530

    Final FIX

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from AKS (Cloud Build)!');
+    res.end('✅ Hello from AKS - SUCCESS!');
 });
 
 server.listen(port, '0.0.0.0', () => {
-    console.log(`✅ Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
